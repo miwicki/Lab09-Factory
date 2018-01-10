e29364f2db953f4cb18ae67bfaba6a18bb052eea
Matt <wrestler.411@comcast.net>
2018-01-10 11:13:12 -0800

    minor changes

--- Doc creator/Doc creator/Degree.cs
+++ Doc creator/Doc creator/Degree.cs
@@ -8,7 +8,6 @@ namespace Product
     {
         public string SchoolName { get; set; } = "School";
         public string DegreeLevel { get; set; } = "Degree";
-        public DateTime GraduationDate { get; set; } = new DateTime(2008, 12, 15);
         public string Major { get; set; } = "Major";
 
         public Education(int pageNumber)
@@ -18,8 +17,8 @@ namespace Product
 
         public override void Print()
         {
-            Console.WriteLine($"Work History: Page {PageNumber}");
-            Console.WriteLine(SchoolName + " - " + GraduationDate.ToString());
+            Console.WriteLine($"Degree: Page {PageNumber}");
+            Console.WriteLine(SchoolName);
             Console.WriteLine(DegreeLevel);
             Console.WriteLine("Major: " + Major);
            
