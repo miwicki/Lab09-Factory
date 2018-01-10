f3cd30bc15c18c5eab7544c41a311784aa24faf1
Matt <wrestler.411@comcast.net>
2018-01-10 10:41:55 -0800

    initial commit

---
+++ Doc creator/Doc creator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Product;
+using Creator;
+
+namespace ResumeCreator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume myResume = new Resume();
+
+            foreach (Page page in myResume.Pages)
+            {
+                page.Print();
+            }
+
+        }
+    }
+}
